ddac34e7f6bef1d4849b000db4b56c4f4d49bdef
umbrien <kalugin.serej@gmail.com>
2021-12-18 11:39:02 +0200

    Score play btn

--- src/components/game/Score.jsx
+++ src/components/game/Score.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Container, Row, Col, Image} from "react-bootstrap";
+import {Container, Row, Col, Image, Button} from "react-bootstrap";
 import {ReactComponent as NearLogo} from "../../assets/near-logo.svg";
 
 export default function Score() {
@@ -11,12 +11,17 @@ export default function Score() {
                 <Image src='/img/yg-logo.png' rounded className='w-50'/>
             </Col>
             <Col className='col-2 text-center'>
-                <h1 className="display-1"><b>:</b></h1>
+                <h1 className="display-1"><b>VS</b></h1>
             </Col>
             <Col className='col-5 text-start'>
                 <Image src='/img/near-logo.png' rounded className='w-50'/>
             </Col>
         </Row>
         <h3 className="text-center my-4">For win you will receive <NearLogo height='30' /> <b>0.5</b></h3>
+        <Row className="justify-content-center">
+            <Col className="col-4 text-center">
+                <Button size='lg' variant='dark' className='text-center w-50'>Play!</Button>
+            </Col>
+        </Row>
     </Container>
 }
